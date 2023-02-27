7d35a43af5f1b7cab48b81b2508bc54c24b1dd8c
Alex <amoahtnt6@gmail.com>
2023-02-27 19:30:45 +0000

    Added some dependencies

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,9 @@ const app: Express = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello Trinity!');
-})
 
 app.use('/api/v1/user', userRouter);
 
-app.listen(5000, () => {
+app.listen(5000, () => { 
     console.log('Listening to port 5000');
 })
